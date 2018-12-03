20bf88adf523e29992efb0acc5f49e5acab230ee
TobbePersson <spara.energi@gmail.com>
2018-12-03 13:27:40 +0100

    Add project files.

---
+++ 1gitRepConsole2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1gitRepConsole2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
